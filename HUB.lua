3d770a56243212ccb99503538ba60901a5b844c5
Seus <81720397+SeusW@users.noreply.github.com>
2021-06-11 16:17:40 -0700

    Update HUB.lua

--- HUB.lua
+++ HUB.lua
@@ -13,10 +13,9 @@ local Main = UI.New({Title = "Main"})
 Main.Slider({Text = "WalkSpeed", Callback = function(value) game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value end, Min = 16, Max = 100, Def = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed})
 Main.Slider({Text = "Jump Power", Callback = function(value) game.Players.LocalPlayer.Character.Humanoid.JumpPower = value end, Min = 50, Max = 150, Def = game.Players.LocalPlayer.Character.Humanoid.JumpPower})
 
-wait(0.5)
-
 if game.PlaceId == 5346447287 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/SeusW/SeusHub/main/TappingGod.lua"))()
 elseif game.PlaceId == 0 then
   
 end
+
