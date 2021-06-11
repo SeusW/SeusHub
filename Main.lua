99c4f2e4b277a2c2e2af24dc7cecfbcaf79d2775
Seus <81720397+SeusW@users.noreply.github.com>
2021-06-11 16:34:00 -0700

    Delete Main.lua

--- Main.lua
+++
@@ -1,4 +0,0 @@
-local Main = UI.New({Title = "Main"})
-
-Main.Slider({Text = "WalkSpeed", Callback = function(value) game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value end, Min = 16, Max = 100, Def = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed})
-Main.Slider({Text = "Jump Power", Callback = function(value) game.Players.LocalPlayer.Character.Humanoid.JumpPower = value end, Min = 50, Max = 150, Def = game.Players.LocalPlayer.Character.Humanoid.JumpPower})
